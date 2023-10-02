ab9c2e25e6f6df93ffea761d377ab39b7bada02b
Mayoka Denis Kitto <92338453+yokasdenis@users.noreply.github.com>
2023-10-02 11:58:05 +0300

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  const message = document.createElement("p"); 
-  message.textContent = "DOM load success";
-  document.body.appendChild(message);
-});
-
